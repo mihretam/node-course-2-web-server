ec0761e069de9e1a2d98d6d6f2ef72511f5fd50e
mihretam <45687915+mihretam@users.noreply.github.com>
2019-02-13 12:28:57 +0100

    Added projects page and adjusted homepage

--- express.js
+++ express.js
@@ -6,21 +6,22 @@ var app = express();
 
 app.set('view engine', 'hbs');
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.get('/', (req, res) => {
 
-    res.send( {
-        name: 'Mihreta',
-        likes: ['node.js', 'movies']
-    });
+    res.render(homepage.hbs);
 } );
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {currentYear: 2019});
 });
 
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs');
+})
  app.get('/bad', (req, res) => {
     res.send({
         error: 404,
